90533d334867d7cd0a2d3aab3df8006f364c119d
zoranlee <lizhangliao@aixhc.com>
2020-03-09 10:21:29 +0800

    修改首页整理代码

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,3 @@
 <script src="./index/script.js"></script>
 <template src="./index/tmpl.html"></template>
-<style src="./index/style.css"></style>
+<style module src="./index/style.css"></style>
---
+++ pages/index/script.js
@@ -0,0 +1,12 @@
+export default {
+  data() {
+    return {
+      title: '珂曼'
+    }
+  },
+  head() {
+    return {
+      title: this.title
+    }
+  }
+}
