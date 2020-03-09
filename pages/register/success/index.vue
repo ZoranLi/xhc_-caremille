56f30ec511981367e4f71d41b9a191778e366015
zoranlee <lizhangliao@aixhc.com>
2020-03-09 14:59:10 +0800

    添加申请成功页面

---
+++ pages/register/success/index.vue
@@ -0,0 +1,3 @@
+<script src="./script.js"></script>
+<style module src="./style.css"></style>
+<template src="./tmpl.html"></template>
---
+++ pages/register/success/script.js
@@ -0,0 +1,14 @@
+export default {
+  head() {
+    return {
+      title: this.title
+    }
+  },
+  data() {
+    return {
+      title: "申请成功"
+    }
+  },
+  
+  
+}
