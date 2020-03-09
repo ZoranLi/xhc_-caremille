df59735826f327b227bedce0a72b611776e0449b
zoranlee <lizhangliao@aixhc.com>
2020-03-09 15:54:29 +0800

    首页抽取

---
+++ components/home/store.vue
@@ -0,0 +1,44 @@
+<style>
+  .template_image {
+    display: block;
+    width: 100%;
+    height: 4.3rem;
+    background-size: cover;
+  }
+</style>
+
+<template>
+  <ul>
+    <li v-for="(image,key) in imageList">
+      <div class='template_image' :data-src="image | optimize('small3x')"></div>
+    </li>
+  </ul>
+</template>
+
+<script>
+
+  import LazyLoad from '~/lib/lazyload'
+  import optimize from '~/filters/imgOptimize'
+
+  export default {
+    data() {
+      return {
+        imageList: [
+          '/images/1562054250687_N45HFE7Ac3.png',
+          '/images/1562054250687_N45HFE7Ac3.png',
+          '/images/1562054250687_N45HFE7Ac3.png',
+          '/images/1562054250687_N45HFE7Ac3.png',
+          '/images/1562054250687_N45HFE7Ac3.png',
+          '/images/1562054250687_N45HFE7Ac3.png',
+        ]
+      }
+    },
+    filters: {
+      optimize
+    },
+    mounted() {
+      this.lazyload = new LazyLoad({selector: 'div[data-src]'})
+    }
+  }
+
+</script>
--- pages/index/script.js
+++ pages/index/script.js
@@ -4,39 +4,26 @@ import {Tabbar, TabbarItem} from 'vant';
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
 import 'vant/lib/index.css';
-
-import LazyLoad from '~/lib/lazyload'
-import optimize from '~/filters/imgOptimize'
+import store from '~/components/home/store'
 
 export default {
   data() {
     return {
       title: '珂曼',
-      imageList: [
-        '/images/1562054250687_N45HFE7Ac3.png',
-        '/images/1562054250687_N45HFE7Ac3.png',
-        '/images/1562054250687_N45HFE7Ac3.png',
-        '/images/1562054250687_N45HFE7Ac3.png',
-        '/images/1562054250687_N45HFE7Ac3.png',
-        '/images/1562054250687_N45HFE7Ac3.png',
-      ],
       active: 'home',
-      icon:{
+      icon: {
         active: '/images/favicon.ico',
         inactive: '/images/close_gray.png'
       }
     }
+  },
+  components: {
+    store
   },
   head() {
     return {
       title: this.title
     }
   },
-  filters: {
-    optimize
-  },
-  mounted() {
-    this.lazyload = new LazyLoad({selector: 'div[data-src]'})
-  }
   
 }
