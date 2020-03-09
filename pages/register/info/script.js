ffe98b3a92b6aa1a774b09f722328b93d76d4f80
zoranlee <lizhangliao@aixhc.com>
2020-03-09 12:23:01 +0800

    引入组件

--- pages/register/info/script.js
+++ pages/register/info/script.js
@@ -1,7 +1,11 @@
+import Vue from 'vue';
+import {DropdownMenu, DropdownItem} from 'vant';
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
 export default {
+  
   async asyncData({$axios, query, store}) {
     return {
-      query,
     }
   },
   head() {
@@ -14,14 +18,22 @@ export default {
       title: '注册VIP账号',
       realName: '',
       storeName: '',
+      value1: 0,
+      value2: 'a',
+      option1: [
+        { text: '全部商品', value: 0 },
+        { text: '新款商品', value: 1 },
+        { text: '活动商品', value: 2 }
+      ],
+      option2: [
+        { text: '默认排序', value: 'a' },
+        { text: '好评排序', value: 'b' },
+        { text: '销量排序', value: 'c' },
+      ]
     }
   },
-  components: {
-  
-  },
-  methods: {
-  
-  },
+  components: {},
+  methods: {},
   mounted() {
   }
 };
