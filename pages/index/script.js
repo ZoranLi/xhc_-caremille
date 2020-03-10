fafec8a84b44b0f61c28fa0eecec98ba3c181593
zoranlee <lizhangliao@aixhc.com>
2020-03-10 15:22:38 +0800

    购物车

--- pages/index/script.js
+++ pages/index/script.js
@@ -3,9 +3,12 @@ import {Tabbar, TabbarItem} from 'vant';
 
 Vue.use(Tabbar);
 Vue.use(TabbarItem);
+
 import 'vant/lib/index.css';
 import store from '~/components/home/store'
 import category from '~/components/home/category'
+import cart from '~/components/home/cart'
+
 
 export default {
   data() {
@@ -20,7 +23,8 @@ export default {
   },
   components: {
     store,
-    category
+    category,
+    cart
   },
   head() {
     return {
