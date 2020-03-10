7011e8e05d72d8191739e6ca654fb7a99f7b4820
zoranlee <lizhangliao@aixhc.com>
2020-03-10 09:52:12 +0800

    注册接口添加

--- pages/register/script.js
+++ pages/register/script.js
@@ -19,20 +19,45 @@ export default {
       title: '注册VIP账号',
       realName: '',
       storeName: '',
-      optionValue: -1,
+      contactAddress: '',
+      optionValue: '',
       options: [
-        {text: '请选择分销渠道', value: -1},
-        {text: '淘宝', value: 0},
-        {text: '京东', value: 1},
-        {text: '拼多多', value: 2},
-        {text: '其他', value: 3}
+        {text: '请选择分销渠道', value: ''},
+        {text: '淘宝', value: 'TB'},
+        {text: '京东', value: 'JD'},
+        {text: '拼多多', value: 'PDD'},
+        {text: '其他', value: 'OTHER'}
       ],
     }
   },
   components: {},
   methods: {
-    submit() {
-    
+    async submit() {
+      if (!this.realName) {
+        return __tostal('请填写真实姓名')
+      }
+      if (!this.storeName) {
+        return __tostal('请填写店铺名称')
+      }
+      if (!this.contactAddress) {
+        return __tostal('请填写联系地址')
+      }
+      
+      if(!this.optionValue){
+        return __tostal('请选择分销渠道')
+      }
+      let params  ={
+        real_name: this.realName,
+        shop_name: this.storeName,
+        contact_address: this.contactAddress,
+        channel: this.optionValue
+      }
+      let resp = await this.$axios.$post(`/v1/agent/distribution`,params);
+      if(resp&&resp.data){ //申请成功
+        location.href = '/register/success'
+      }else if(resp.msg){
+        __tostal(resp.msg)
+      }
     }
   },
   mounted() {
